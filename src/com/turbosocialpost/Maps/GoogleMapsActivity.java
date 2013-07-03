0bc0a54333068b6a502a3d2dfaf0cd23ea80bbf7
BernEugen <berneugen@gmail.com>
2013-07-03 08:12:15 +0300

    added maps support with google play service r5

--- src/com/turbosocialpost/Maps/GoogleMapsActivity.java
+++ src/com/turbosocialpost/Maps/GoogleMapsActivity.java
@@ -2,11 +2,8 @@ package com.turbosocialpost.Maps;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -14,9 +11,14 @@ import com.turbosocialpost.R;
 
 public class GoogleMapsActivity extends Activity {
 
+    GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps_layout);
+
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        googleMap.setMyLocationEnabled(true);
     }
 }
